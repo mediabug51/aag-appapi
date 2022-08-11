91215b3c531b0418bba13282a2afa10ac7e64ff9
nanak <nanaksethh@gmail.com>
2022-08-11 21:02:11 +0530

    zdzsd

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,16 @@
-const path = require('path');
-const parse= require('pg-connection-string').parse
-const config=parse(process.env.DATABASE_URL)
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host:config.host,
+      host: config.host,
       port: config.port,
-      database:config.database,
+      database: config.database,
       user: config.user,
       password: config.password,
       ssl: {
-        rejectUnauthorized: false,
+        rejectUnauthorized: false
       },
     },
     debug: false,
