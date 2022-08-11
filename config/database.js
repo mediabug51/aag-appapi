49e3ae444e71383cd7517035c3217a8c9a324e06
nanak <nanaksethh@gmail.com>
2022-08-11 19:10:35 +0530

    Changes

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const parse= require('pg-connection-string')
 const config=parse(process.env.DATABASE_URL)
+console.log(config)
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
